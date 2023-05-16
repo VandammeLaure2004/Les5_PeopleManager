901abf4706c932d630253d92155fe8f4d5db1ad2
VandammeLaure2004 <125573527+VandammeLaure2004@users.noreply.github.com>
2023-05-16 08:42:43 +0200

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Core/PeopleManagerDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Core
+{
+    public class PeopleManagerDbContext: DbContext
+    {
+
+        public PeopleManagerDbContext(DbContextOptions<PeopleManagerDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Person> People => Set<Person>();
+
+        public void Seed()
+        {
+            People.AddRange(new List<Person>
+            {
+                new Person
+                {
+                    FirstName = "Bavo",
+                    LastName = "Ketels",
+                    Email = "bavo.ketels@vives.be",
+                    Description = "Lector"
+                },
+                new Person{FirstName = "Isabelle", LastName = "Vandoorne", Email = "isabelle.vandoorne@vives.be" },
+                new Person
+                {
+                    FirstName = "Wim",
+                    LastName = "Engelen",
+                    Email = "wim.engelen@vives.be",
+                    Description = "Opleidingshoofd"
+                },
+                new Person{FirstName = "Ebe", LastName = "Deketelaere", Email = "ebe.deketelaere@vives.be" }
+            });
+
+            SaveChanges();
+        }
+    }
+}
---
+++ PeopleManager.Ui.Mvc/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeopleManager.Ui.Mvc.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Display(Name="First Name")]
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public required string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [Required]
+        public required string Email { get; set; }
+
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+    }
+}
